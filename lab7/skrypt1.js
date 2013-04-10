0cccf42a0f1277479c269d426f69d9f5dfb291bc
Mateusz Motel <mateuszmotel@gmail.com>
2013-04-10 20:23:35 +0200

    changed color ligthning function in 7th lab's excercise

--- lab7/skrypt1.js
+++ lab7/skrypt1.js
@@ -1,13 +1,23 @@
 $(function () {
-	var getColour = function (i) { // "brzydkie" i mało eleganckie rozwiązanie 
-		var color;
-		if(i === 1) { color = '#bbbbbb'; }
-		else if(i === 2) { color = '#cccccc'; }
-		else if(i === 3) { color = '#dddddd'; }
-		else if(i === 4) { color = '#eeeeee'; }
-		else { color = '#ffffff'; }
-		return color;
-	};
+	var lighten = function(color, percent) { //funkcja od Macieja Smefju Małeckiego (https://github.com/smt116)
+		var R = parseInt(color.substring(1,3),16);
+		var G = parseInt(color.substring(3,5),16);
+		var B = parseInt(color.substring(5,7),16);
+
+		R = parseInt(R * (100 + percent) / 100);
+		G = parseInt(G * (100 + percent) / 100);
+		B = parseInt(B * (100 + percent) / 100);
+
+		R = (R<255)?R:255;  
+		G = (G<255)?G:255;  
+		B = (B<255)?B:255;  
+
+		var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+		var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+		var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+		return '#' + RR + GG + BB;
+	}
 	
 	var children = $(document.documentElement).children();
 	while(children.length > 0){
@@ -17,7 +27,7 @@ $(function () {
 				$(this).css('background', '#aaaaaa');
 				var parent = $(this).parent();
 				while($(parent).length > 0){					
-					parent.css('background', getColour(i));
+					parent.css('background', lighten('#aaaaaa', i * 10));
 					parent = $(parent).parent();
 					i++;
 				}
