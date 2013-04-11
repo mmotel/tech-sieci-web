42106cadcd33d8d6fa3ce8155a89d056865d8f95
Mateusz Motel <mateuszmotel@gmail.com>
2013-04-11 13:21:59 +0200

    added 6th lab's excercise 4

---
+++ lab6/skrypt_zad4.js
@@ -0,0 +1,19 @@
+$(function() {
+	var cenzura = function (patt){
+		var pattern = new RegExp(patt, 'g');
+		var children = $(document.documentElement).children();
+		while(children.length > 0){
+			for(var i=0; i < $(children).length; i+=1){
+				if(children[i].nodeType === 3){
+					var m = children[i].data.match(pattern);
+					if(m){
+						children[i].data = children[i].data.replace(pattern, 'CENZURA');
+						$(children[i]).parent().css('background', 'red');
+					}
+				}
+			}
+            children = $(children).contents();
+        }
+	};
+	cenzura('treść');
+});
